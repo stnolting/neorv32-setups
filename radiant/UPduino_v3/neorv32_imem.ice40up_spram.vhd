6f86dd262f83f9c61cb31475e8476d96f06dd8d3
stnolting <stnolting@gmail.com>
2022-02-07 09:03:06 +0100

    added err_i sginal to ice40 SPRAM IMEM modules

--- radiant/UPduino_v3/neorv32_imem.ice40up_spram.vhd
+++ radiant/UPduino_v3/neorv32_imem.ice40up_spram.vhd
@@ -6,7 +6,7 @@
 -- # ********************************************************************************************* #
 -- # BSD 3-Clause License                                                                          #
 -- #                                                                                               #
--- # Copyright (c) 2021, Stephan Nolting. All rights reserved.                                     #
+-- # Copyright (c) 2022, Stephan Nolting. All rights reserved.                                     #
 -- #                                                                                               #
 -- # Redistribution and use in source and binary forms, with or without modification, are          #
 -- # permitted provided that the following conditions are met:                                     #
@@ -138,6 +138,9 @@ begin
       rden  <= acc_en and rden_i;
     end if;
   end process buffer_ff;
+
+  -- no access error --
+  err_o <= '0';
 
   -- output gate --
   data_o <= rdata when (rden = '1') else (others => '0');
